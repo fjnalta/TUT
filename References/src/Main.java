58db60493882ea9075ee5d14552c952b6fb25d36
Philipp Minges <fjnalta@gmail.com>
2017-10-10 20:34:29 +0200

    refactor packages2

--- References/src/Main.java
+++ References/src/Main.java
@@ -5,10 +5,23 @@ public class Main {
         String a = "Hallo";
         String b = "Welt";
 
+        int x = 5;
+        int y = 9;
+
         swap(a,b);
 
         System.out.println(a + " " + b);
 
+        swap(x,y);
+
+        System.out.println(x + " " + y);
+
+    }
+
+    private static void swap(int x, int y) {
+        int tmp = x;
+        x = y;
+        y = tmp;
     }
 
     private static void swap(String a, String b) {
